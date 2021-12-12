ee9c041831e0e706a5551d8ceb719a8e96d766e7
Daniel <daniel.siegmanski@gmail.com>
2021-12-12 11:44:16 +0100

    Inital Release

---
+++ enigma2xmltv.py
@@ -0,0 +1,138 @@
+import urllib.request
+import datetime
+import xml.etree.cElementTree as ET
+import os.path
+
+class enigma2xmltv():
+    def __init__(self, server, lang: str = "de"):
+        '''
+        server: IP oder Hostname der des enigma webinterfaces
+        lang: Länderkürzel deiner Sprache
+        '''
+        self.server = "http://" + server + "/web/epgservice?sRef="
+        self.lang = lang
+        self.channels = []
+        self.raw_epg = {}
+        self.epg = {}
+
+    def add_channel(self, *channels):
+        '''
+        Kanäle zum grabben hinzufügen.
+        *channels: Kanal 1, Kanal 2, ...
+        '''
+        for c in channels:
+            self.channels.append(c)
+
+    def show_channels(self):
+        '''
+        Gibt alle hinzugefügten Kanäle zurück.
+        '''
+        print(self.channels)
+
+    def del_channel(self, id):
+        '''
+        Löscht einen Kanal aus der Liste.
+        id: Wenn ein integer wird der entsprechende Index gelöscht
+        id: Wenn ein string wird der entsprechende Kanal gelöscht
+        '''
+        if ( type(id) == int ):
+            try:
+                self.channels.pop(id)
+            except:
+                pass
+        elif ( type(id) == str):
+            try:
+                self.channels.remove(id)
+            except:
+                pass
+
+    def load_channel_epg(self, id):
+        '''
+        Läd das EPG eines Kanals, der über add_channel hinzugefügt wurde.
+        name: Name des Kanals, desen EPG geladen werden soll.
+        '''
+        self.raw_epg[id] = urllib.request.urlopen(self.server + id, timeout=3).read().decode('utf8')
+
+    def load_epg(self):
+        '''
+        Läd das EPG aller Kanäle die über add_channel hinzugefügt wurden herunter.
+        '''
+        for c in self.channels:
+            self.load_channel_epg(c)
+
+    def convert_channel(self, channel):
+        '''
+        Konvertiert das EPG eines Kanals in das XMLTV-Format
+        name: Name des Kanals
+        '''
+        if ( channel in self.raw_epg ):
+            if "e2eventservicename" not in self.raw_epg[channel]:
+                # Das scheint kein gültiges EPG zu sein.
+                # Gibt es den Kanal vielleicht nicht?
+                self.raw_epg[channel] = None
+            
+            myroot = ET.fromstring(self.raw_epg[channel])
+            myxml = ET.Element("tv", {"source-info-name": "Enigma2XMLTV"})
+
+            for x in range(0,len(myroot)):
+                name = ""
+                desc = ""
+                descext = ""
+                title = ""
+                start = ""
+                end = ""
+                duration = ""
+
+                for e in myroot[x]:
+                    if e.tag == "e2eventservicename":
+                        if e.text is not None:
+                            name = e.text
+                    elif e.tag == "e2eventtitle":
+                        if e.text is not None:
+                            title = e.text
+                    elif e.tag == "e2eventdescription":
+                        if e.text is not None:
+                            desc = e.text
+                    elif e.tag == "e2eventdescriptionextended":
+                        if e.text is not None:
+                            descext = e.text
+                    elif e.tag == "e2eventstart":
+                        if e.text is not None:
+                            start = datetime.datetime.fromtimestamp(int(e.text)).strftime('%Y%m%d%H%M%S')
+                    elif e.tag == "e2eventduration":
+                        if e.text is not None:
+                            duration = int(e.text)
+                            end = datetime.datetime(int(start[:4]), int(start[4:6]), int(start[6:8]), int(start[8:10]), int(start[10:12]), int(start[12:14])) \
+                                - datetime.timedelta(seconds=duration)
+                            end = end.strftime('%Y%m%d%H%M%S')
+
+                myprog = ET.SubElement(myxml, "programme", {'name': name, 'start': start, 'stop': end} )
+                ET.SubElement(myprog, "title", { "lang": self.lang }).text = title
+                ET.SubElement(myprog, "desc", { "lang": self.lang }).text = desc + " -- " + descext
+                ET.SubElement(myprog, "category").text = ""
+
+            # Hinzufügen der Kanalinfos
+            mychannel = ET.SubElement(myxml, "channel", {"id": channel} )
+            ET.SubElement(mychannel, "display-name", {"lang": self.lang}).text = name
+
+            self.epg[channel] = ET.ElementTree(myxml)
+
+    def convert_channels(self):
+        '''
+        Konvertiert das EPG aller Kanäle in das XMLTV-Format
+        '''
+        for e in self.raw_epg:
+            self.convert_channel(e)
+
+    def save_epg(self, path: str):
+        '''
+        Speichert das EPG im XMLTV-Format ab.
+        path: Pfad in dem die Datei(en) gespeichert werden sollen.
+        '''
+        if ( not os.path.exists(path) ):
+            raise OSError("Pfad " + path + " existiert nicht.")
+
+        for e in self.epg:
+            self.epg[e].write(path + "/" + e + ".xml", encoding='utf-8')
+
+
---
+++ example.py
@@ -0,0 +1,46 @@
+from enigma2xmltv import enigma2xmltv
+
+# Init mit der IP des Receivers
+x = enigma2xmltv("192.168.0.115")
+
+# Mehrere Sender hinzufügen
+# Das Erste HD
+x.add_channel("1%3A0%3A19%3A2B5C%3A41B%3A1%3AFFFF0000%3A0%3A0%3A0%3A")
+# ZDF HD
+x.add_channel("1%3A0%3A19%3A2B66%3A437%3A1%3AFFFF0000%3A0%3A0%3A0%3A")
+# Tele 5 HD & Comedy Central
+x.add_channel("1%3A0%3A19%3AC37B%3A2719%3AF001%3AFFFF0000%3A0%3A0%3A0%3A",
+                "1%3A0%3A19%3AC382%3A271A%3AF001%3AFFFF0000%3A0%3A0%3A0%3A")
+
+# Entfernen von ZDF HD
+x.del_channel("1%3A0%3A19%3A2B66%3A437%3A1%3AFFFF0000%3A0%3A0%3A0%3A")
+
+# ##############################
+# Beispiel zum laden des EPGs aller Sender
+# ##############################
+
+# EPG aller Sender laden
+x.load_epg()
+
+# EPG aller geladenen Sender konvertieren
+x.convert_channels()
+
+# Abspeichern der EPG-Daten im XMLTV-Format
+x.save_epg(".")
+
+# ##############################
+# Beispiel zum laden des EPGs von bestimmten Sendern
+# ##############################
+
+# EPG von Das Erste HD laden
+x.load_channel_epg("1%3A0%3A19%3A2B5C%3A41B%3A1%3AFFFF0000%3A0%3A0%3A0%3A")
+# EPG von Comedy Central laden
+x.load_channel_epg("1%3A0%3A19%3AC382%3A271A%3AF001%3AFFFF0000%3A0%3A0%3A0%3A")
+
+# EPG aller geladenen Sender konvertieren (Das Erste HD & Comedy Central)
+x.convert_channels()
+# Oder nur das EPG eines bestimmten Senders konvertieren (Das Erste HD)
+x.convert_channel("1%3A0%3A19%3A2B5C%3A41B%3A1%3AFFFF0000%3A0%3A0%3A0%3A")
+
+# Abspeichern der EPG-Daten im XMLTV-Format
+x.save_epg(".")
